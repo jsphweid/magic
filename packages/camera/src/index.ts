2c1cc969d30b8c695e8557130cecc534e07633b2
Todd Elvers <toddelvers@carfax.com>
2018-07-29 19:11:48 -0500

    Removed unnecessary if-condition

--- packages/camera/src/index.ts
+++ packages/camera/src/index.ts
@@ -1,32 +1,30 @@
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  const video: HTMLVideoElement = document.getElementById(
-    "video"
-  ) as HTMLVideoElement;
+const video: HTMLVideoElement = document.getElementById(
+  "video"
+) as HTMLVideoElement;
 
-  const videoSettings: MediaStreamConstraints = {
-    video: true,
-    audio: true
-  };
+const videoSettings: MediaStreamConstraints = {
+  video: true,
+  audio: true
+};
 
-  navigator.mediaDevices
-    .getUserMedia(videoSettings)
-    .then((stream: MediaStream): void => {
-      video.setAttribute("src", window.URL.createObjectURL(stream));
-      video.play();
-    });
+navigator.mediaDevices
+  .getUserMedia(videoSettings)
+  .then((stream: MediaStream): void => {
+    video.setAttribute("src", window.URL.createObjectURL(stream));
+    video.play();
+  });
 
-  const canvas: HTMLCanvasElement = document.getElementById(
-    "canvas"
-  ) as HTMLCanvasElement;
-  const context: CanvasRenderingContext2D = canvas.getContext("2d");
+const canvas: HTMLCanvasElement = document.getElementById(
+  "canvas"
+) as HTMLCanvasElement;
+const context: CanvasRenderingContext2D = canvas.getContext("2d");
 
-  // Automatic screenshots
-  // setInterval(() => {
-  //   context.drawImage(video, 0, 0, 640, 480);
-  // }, 1000);
+// Automatic screenshots
+// setInterval(() => {
+//   context.drawImage(video, 0, 0, 640, 480);
+// }, 1000);
 
-  // Manual screenshots via button
-  // document.getElementById("snap").addEventListener("click", (): void => {
-  //   context.drawImage(video, 0, 0, 640, 480);
-  // });
-}
+// Manual screenshots via button
+// document.getElementById("snap").addEventListener("click", (): void => {
+//   context.drawImage(video, 0, 0, 640, 480);
+// });
