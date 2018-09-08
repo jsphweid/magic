1ea0d58dcc22b437388d05e789759c70e8280674
Conner Ruhl <connerruhl@me.com>
2018-09-07 20:46:32 -0500

    Refactoring data display since it was untyped and awful

--- packages/api/src/Message/Reply/Data.ts
+++ packages/api/src/Message/Reply/Data.ts
@@ -1,68 +1,62 @@
-export const toString = (
-  data: any,
-  indent: string = "",
+import _ from "lodash";
+// import wrap from "word-wrap";
+
+const MAX_WIDTH = 23;
+
+export type JSONValue = JSONObject | JSONArray | JSONPrimitive;
+
+export interface JSONObject {
+  [key: string]: JSONValue;
+}
+
+export interface JSONArray extends Array<JSONValue> {}
+export type JSONPrimitive = string | number | boolean | null;
+
+export const toString = (data: JSONValue): string => withIndent("", data);
+
+export const withIndent = (
+  indent: string,
+  value: JSONValue,
   addIndent: boolean = false
-): string =>
-  isObject(data)
-    ? objectToString(data, `${indent}${addIndent ? " " : ""}`)
-    : typeof data !== "string"
-      ? JSON.stringify(data)
-      : data;
-
-const objectToString = (
-  object: any[] | { [key: string]: any },
-  indent: string
 ): string => {
-  if (Array.isArray(object)) {
-    const values = object.map(value => {
-      return isObject(value)
-        ? objectToString(value, indent)
-        : toString(value, indent, true);
-    });
-
-    const asLine = values.join(" ");
-    if (isEmpty(asLine)) {
-      return "";
-    }
-
-    return !isTooLong(indent, asLine)
-      ? asLine
-      : values
-          .map(value => `${!isIndented(indent, value) ? indent : ""}${value}`)
-          .join(`\n`);
-  }
-
-  const nonNulls = Object.entries(object).filter(([, value]) => value !== null);
-  if (nonNulls.length === 1) {
-    const [[, childValue]] = nonNulls;
-    return toString(childValue, indent);
-  }
-
-  const fields: string[] = nonNulls
-    .map(([key, value]) => {
-      const contents = toString(value, indent, true);
-      if (isEmpty(contents)) {
-        return "";
-      }
-
-      const spacing = isTooLong(indent, key, " ", contents) ? "\n" : " ";
-      return `${key}${spacing}${contents}`;
-    })
-    .filter(value => !isEmpty(value));
-
-  return objectToString(fields, indent);
+  const updatedIndent = `${indent}${addIndent ? " " : ""}`;
+  return isObject(value)
+    ? objectToString(updatedIndent, value)
+    : Array.isArray(value)
+      ? arrayToString(updatedIndent, value)
+      : primitiveToString(updatedIndent, value);
 };
 
-const isObject = (value: any): boolean => value && typeof value === "object";
+const isObject = (value: JSONValue): value is JSONObject =>
+  value !== null && typeof value === "object";
 
-const isTooLong = (...strings: string[]): boolean =>
-  strings.join("").length > 23;
+const objectToString = (
+  indent: string,
+  object: { [key: string]: any }
+): string =>
+  arrayToString(
+    indent,
+    Object.entries(object).map(
+      ([key, value]) => `${key} ${withIndent(indent, value, true)}`
+    )
+  );
 
-const isIndented = (indent: string, string: string): boolean =>
-  string.indexOf(indent) === 0;
+const arrayToString = (indent: string, array: JSONArray): string => {
+  // const asOneLine = array.map(value => withIndent("", value)).join(" ");
+  // if (!isTooLong(asOneLine)) {
+  //   return asOneLine;
+  // }
 
-const isEmpty = (string: string): boolean =>
-  string.replace(/\n/g, "").trim() === "";
+  return array.map(value => `${withIndent(indent, value)}`).join("");
+};
+
+const primitiveToString = (indent: string, primitive: JSONPrimitive): string =>
+  typeof primitive === "string"
+    ? `\n${indent}${primitive}`
+    : // wrap(primitive, { indent, width: MAX_WIDTH })
+      JSON.stringify(primitive);
+
+const isTooLong = (string: string): boolean => string.length > MAX_WIDTH;
 
 const x = toString({
   data: {
@@ -87,8 +81,29 @@ const x = toString({
             stop: null
           },
           tag: {
-            name: "magic",
-            score: "POSITIVE_HIGH"
+            name: "magic"
+          }
+        },
+        {
+          interval: {
+            start: {
+              formatted: "3:09 PM Fr Sep 7"
+            },
+            stop: null
+          },
+          tag: {
+            name: "todd-elvers"
+          }
+        },
+        {
+          interval: {
+            start: {
+              formatted: "3:09 PM Fr Sep 7"
+            },
+            stop: null
+          },
+          tag: {
+            name: "weed"
           }
         }
       ]
